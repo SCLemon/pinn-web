6fd7a03367fd15b8e17fa0eefc0023de31c9a84b
SC1314520 <g0710126@hchs.hc.edu.tw>
2024-10-11 13:28:56 +0800

    刪除覆蓋 main 函式

--- backend/routes/runRouter.js
+++ backend/routes/runRouter.js
@@ -70,11 +70,11 @@ router.post('/run/upload',upload.fields([
     fs.writeFileSync(destinationFilePath,src.buffer,'utf8')
 
     /* 覆蓋 main.py 開始 */ // 之後要刪掉
-    const aneurysmPath = path.join(__dirname, '/static/aneurysm.py');
-    const copyToPath = path.join(__dirname, fPath, `${uuid}.py`);
-    fs.readFile(aneurysmPath, 'utf8', (err, data) => {
-        fs.writeFileSync(copyToPath, data, 'utf8');
-    })
+    // const aneurysmPath = path.join(__dirname, '/static/aneurysm.py');
+    // const copyToPath = path.join(__dirname, fPath, `${uuid}.py`);
+    // fs.readFile(aneurysmPath, 'utf8', (err, data) => {
+    //     fs.writeFileSync(copyToPath, data, 'utf8');
+    // })
     /* 覆蓋 main.py 結束 */
 
     var name = `${format(new Date(),'HHmm')}_project`;
